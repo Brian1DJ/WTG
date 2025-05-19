0f6529c7b4ed8b10ba1bbe355e722c2a8d080cfc
Brian1DJ <brianmathewdejesus@gmail.com>
2025-05-20 06:05:15 +0800

    new

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::view('/', 'landing');
 
 Route::view('/home', 'homepage');
 
 Route::view('/contacts', 'contacts.index');
-
-
+Route::get('/search-page', function () {
+    return view('account-view.search-page');
+});
+Route::get('/search', [EventController::class, 'search'])->name('events.search');
 Route::view('/help-center', 'helpcenter.index');
 
 Route::view('/account/create', 'account.create');
